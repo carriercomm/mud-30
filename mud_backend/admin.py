8f195d3048f88d5fd10b5ea1d6eaef6409d17c4b
Chip Bell <chip.bell@spinen.com>
2015-11-27 19:03:35 -0500

    Enabling in the admin

--- mud_backend/admin.py
+++ mud_backend/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Room, Item, Action, SaveSlotActionCriteria, InventoryActionCriteria, SaveSlotActionResult, InventoryActionResult, RoomActionResult
+
+admin.site.register(Room)
+admin.site.register(Item)
+admin.site.register(Action)
+admin.site.register(SaveSlotActionCriteria)
+admin.site.register(InventoryActionCriteria)
+admin.site.register(SaveSlotActionResult)
+admin.site.register(InventoryActionResult)
+admin.site.register(RoomActionResult)
