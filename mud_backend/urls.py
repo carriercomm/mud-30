893f1fee417c3a375191d74c467c522dba99fc69
Chip Bell <chip.bell@spinen.com>
2015-12-06 15:26:49 -0500

    Moving the route to a better name

--- mud_backend/urls.py
+++ mud_backend/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 
-from . import views
+from . import actions
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^actions$', actions.index, name='index'),
 ]
