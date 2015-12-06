52f9233db9e7d26cfd0c872f0b9c85fef9e0bef6
Chip Bell <chip.bell@spinen.com>
2015-12-06 15:18:52 -0500

    Simpler linting

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -e
-python ~/.pyenv/versions/3.4.1/bin/pep8 mud_backend/migrations/0002_auto_20151128_1652.py mud_backend/*.py
+python ~/.pyenv/versions/3.4.1/bin/pep8 mud_backend/*.py
