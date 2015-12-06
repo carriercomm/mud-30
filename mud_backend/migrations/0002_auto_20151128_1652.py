6c2b7c8eb67f3454f2eb2c19da4245caf69514c4
Chip Bell <chip.bell@spinen.com>
2015-12-06 15:12:26 -0500

    Renaming a variable. A little spacing too

--- mud_backend/migrations/0002_auto_20151128_1652.py
+++ mud_backend/migrations/0002_auto_20151128_1652.py
@@ -5,13 +5,14 @@ from django.db import migrations, models
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import make_password
 
+
 def stub_map(apps, schema_editor):
     # create some rooms
     Room = apps.get_model('mud_backend', 'Room')
     entrance_room = Room(description='You entered a castle. There is a key on the ground and a door north of you')
     entrance_room.save()
-    second_room = Room(description='You are in the study. Ancient books surround you, but there\'s nothing else. The foyer is south of you')
-    second_room.save()
+    study = Room(description='You are in the study. Ancient books surround you, but there\'s nothing else. The foyer is south of you')
+    study.save()
 
     # create the user profile to put the user in the entrance room
     user = User(username='testuser', first_name='test', last_name='user', is_superuser=True, is_staff=True, password=make_password('password'))
@@ -34,10 +35,10 @@ def stub_map(apps, schema_editor):
     foyer_to_study = Action(message='You entered the study', room_id=entrance_room.id, matcher='(go north|enter door)')
     foyer_to_study.save()
     InventoryActionCriteria(action_id=foyer_to_study.id, order=1, item_id=key.id, should_have=True, error_message='The door is locked. Looks like you need a key').save()
-    RoomActionResult(action_id=foyer_to_study.id, room_id=second_room.id).save()
+    RoomActionResult(action_id=foyer_to_study.id, room_id=study.id).save()
 
     # create the action for going from the study to the foyer
-    study_to_foyer = Action(message='You left the study and are now in the foyer again', room_id=second_room.id, matcher='(exit|leave study|go south)')
+    study_to_foyer = Action(message='You left the study and are now in the foyer again', room_id=study.id, matcher='(exit|leave study|go south)')
     study_to_foyer.save()
     RoomActionResult(action_id=study_to_foyer.id, room_id=entrance_room.id).save()
 
